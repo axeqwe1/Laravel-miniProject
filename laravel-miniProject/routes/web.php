a0970e89a15ce3f1c2bab8d351f383901da8a574
axeqwe1 <shuffer2543@gmail.com>
2022-09-25 14:13:02 +0700

    create_layout

--- laravel-miniProject/routes/web.php
+++ laravel-miniProject/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\OrdersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('order', OrdersController::class);
